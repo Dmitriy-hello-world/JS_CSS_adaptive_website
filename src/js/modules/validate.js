060b290b2fcd00956c656939e0c245469e40d106
Dmitriy <easportstng@gmail.com>
2021-04-13 21:52:08 +0300

    some update validate code + add comments

--- src/js/modules/validate.js
+++ src/js/modules/validate.js
@@ -1,20 +1,28 @@
 function validate(mainForms, formName, formPhone, formEmail) {
-    const forms = document.querySelectorAll(mainForms);
 
+    const forms = document.querySelectorAll(mainForms);
     let formError = true;
 
+    //add listener for all forms on page
+
     forms.forEach(form => {
+        // get input 'name' , 'phone' , 'email' in each form
         const name = form.querySelector(formName),
             phone = form.querySelector(formPhone),
-            email = form.querySelector(formEmail);
+            email = form.querySelector(formEmail),
+            // regular expressions for validate
+            nameReg = /^[a-zа-я_-]{2,16}$/i,
+            phoneReg = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/,
+            emailReg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+        //function to create error message and check validate name
         function checkValidName() {
 
             let nameError = document.createElement('div');
 
             name.addEventListener('input', function () {
 
-                let validName = this.value.match(/^[a-zа-я_-]{2,16}$/i);
+                let validName = this.value.match(nameReg);
 
                 nameError.remove();
                 this.style.border = '';
@@ -36,13 +44,14 @@ function validate(mainForms, formName, formPhone, formEmail) {
             });
         }
 
+        //function to create error message and check validate phone
         function checkValidPhone() {
 
             let phoneError = document.createElement('div');
 
             phone.addEventListener('input', function () {
 
-                let validPhone = this.value.match(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/);
+                let validPhone = this.value.match(phoneReg);
 
                 phoneError.remove();
                 this.style.border = '';
@@ -65,13 +74,14 @@ function validate(mainForms, formName, formPhone, formEmail) {
 
         }
 
+        //function to create error message and check validate email
         function checkValidMail() {
 
             let mailError = document.createElement('div');
 
             email.addEventListener('input', function () {
 
-                let validMail = this.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+                let validMail = this.value.match(emailReg);
 
                 mailError.remove();
                 this.style.border = '';
@@ -98,17 +108,18 @@ function validate(mainForms, formName, formPhone, formEmail) {
         checkValidName();
         checkValidPhone();
 
+        //checking browser autocomplite before sent data to server
         form.addEventListener('submit', Event => {
             if (formError || name.value == '' || phone.value == '' || email.value == '') {
                 Event.preventDefault();
             }
-            if (name.value.match(/^[a-zа-я_-]{2,16}$/i) == null) {
+            if (name.value.match(nameReg) == null) {
                 Event.preventDefault();
             }
-            if (phone.value.match(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/) == null) {
+            if (phone.value.match(phoneReg) == null) {
                 Event.preventDefault();
             }
-            if (email.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) == null) {
+            if (email.value.match(emailReg) == null) {
                 Event.preventDefault();
             }
         });
